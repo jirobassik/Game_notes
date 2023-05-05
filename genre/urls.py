a33c6ce615cfc6eb0e5bc3a6f1266451c07f69e0
= <jirobassik@tut.by>
2023-05-05 13:45:55 +0300

    Add CRUD for genre

--- genre/urls.py
+++ genre/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from game.views import view_game
 
 urlpatterns = [
     path('', views.view_genres, name="genre"),
-    # path('view_game/<int:pk>', views.GenreView.as_view(), name='genre-view_game'),
-    path('delete_game/<int:pk>', views.delete_genre, name='genre-delete_game'),
-    # path('edit_game/<int:pk>', views.GenreUpdate.as_view(), name='genre-edit_game'),
+    path('view_game/<int:pk>', views.detail_view_genres, name='genre-view'),
+    path('delete/<int:pk>', views.delete_genre, name='genre-delete'),
+    path('edit/<int:pk>', views.edit_genre, name='genre-edit'),
     path('create/', views.add_genre, name='create-genre'),
 ]
--- genre/views.py
+++ genre/views.py
@@ -1,21 +1,10 @@
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
-from django.contrib import messages
-from .models import GameGenreModel
-# from .forms import GenreForm
-
-
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse_lazy, reverse
-from .models import GameGenreModel
+from django.urls import reverse
+
 from .forms import GameGenreForm
 from genre.models import GameGenreModel
-from genre.serializers import GenreSerializer
 
-from rest_framework.renderers import TemplateHTMLRenderer
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from utils.request_server import Request
 from utils.json_serializer import JsonSerializer
 from .serializers import GenreSerializer
@@ -23,55 +12,50 @@ from .serializers import GenreSerializer
 genre_json_serializer = JsonSerializer(GameGenreModel, GenreSerializer)
 genre_request = Request.genre_model()
 
+
 def add_genre(request):
-    standart_form = GameGenreForm
-    return render(request, 'genre/create_genre.html', {'form': standart_form})
+    if request.method == 'POST':
+        form = GameGenreForm(request.POST)
+        if form.is_valid():
+            serializer_data = genre_json_serializer.encode(name=form.cleaned_data['name'],
+                                                           description=form.cleaned_data['description'], )
+
+            genre_request.post_request(serializer_data)
+        return HttpResponseRedirect(reverse('genre'))
+
+    else:
+        form = GameGenreForm()
+    return render(request, 'genre/create_genre.html', {'form': form})
+
+
+def edit_genre(request, pk=None):
+    raw_data_platform = genre_request.detail_get_request(pk)
+    queryset_platform = genre_json_serializer.detail_decode(raw_data_platform)
+    if request.method == 'POST':
+        form = GameGenreForm(request.POST)
+        if form.is_valid():
+            serializer_data = genre_json_serializer.encode(name=form.cleaned_data['name'],
+                                                           description=form.cleaned_data['description'], )
+
+            genre_request.put_request(serializer_data, pk)
+        return HttpResponseRedirect(reverse('genre'))
+    else:
+        form = GameGenreForm(initial=queryset_platform)
+    return render(request, 'genre/update_genre.html', {'form': form})
+
 
 def view_genres(request):
     raw_data_genre = genre_request.get_request()
     queryset_genre = genre_json_serializer.decode(raw_data_genre)
     return render(request, 'genre/genre.html', {'genres': queryset_genre})
 
+
+def detail_view_genres(request, pk=None):
+    raw_data = genre_request.detail_get_request(pk)
+    queryset = genre_json_serializer.detail_decode(raw_data)
+    return render(request, 'genre/view_genre.html', {'platform': queryset})
+
+
 def delete_genre(request, pk=None):
     genre_request.delete_request(pk)
     return HttpResponseRedirect(reverse('genre'))
-
-
-
-
-
-
-
-#
-# class Genre(ListView):
-#     model = GameGenreModel
-#     template_name = 'genre/genre.html'
-#     context_object_name = 'genres'
-#
-# class GenreView(DetailView):
-#     model = GameGenreModel
-#     template_name = 'genre/view_genre.html'
-#     context_object_name = 'genre'
-#
-# class GenreCreate(CreateView):
-#     model = GameGenreModel
-#     form_class = GenreForm
-#     template_name = 'genre/create_genre.html'
-#     success_url = reverse_lazy("genre")
-#
-#
-# class GenreUpdate(UpdateView):
-#     model = GameGenreModel
-#     form_class = GenreForm
-#     template_name = 'genre/update_genre.html'
-#     success_url = reverse_lazy("genre")
-#
-#
-# class GenreDelete(DeleteView):
-#     model = GameGenreModel
-#     context_object_name = 'genre'
-#     success_url = reverse_lazy("genre")
-#
-#     def form_valid(self, form):
-#         messages.success(self.request, "Жанр был успешно удален.")
-#         return super(GenreDelete, self).form_valid(form)
